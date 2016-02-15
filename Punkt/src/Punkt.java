dd804b2851e786b999be6857b17ad186f75333a4
Asus <zacniewski@gmail.com>
2016-02-15 23:36:07 +0100

    First commit

---
+++ Punkt/src/Punkt.java
@@ -0,0 +1,29 @@
+
+public class Punkt {
+	public int x;
+	private int y;
+
+	
+
+	public void Wyswietl(){
+		System.out.println("Wspolrzedne wierzcholka to(" + this.x + "," + this.y +").");
+	}
+	
+
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+		
+}
---
+++ Punkt/src/Test.java
@@ -0,0 +1,18 @@
+
+public class Test {
+	public static void main(String[] args) {
+		Punkt a = new Punkt();
+		Punkt b = new Punkt();
+		
+		//Punkt a
+		a.x=1;
+		a.setY(5);
+		
+		//Punkt b
+		b.x=5;
+		b.setY(3);
+		
+		a.Wyswietl();
+		b.Wyswietl();
+	}
+}
